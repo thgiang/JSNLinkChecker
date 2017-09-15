732fd6365116525d7f55493c7d3d7b32d8d72f7c
Tran Hoang Giang <tranhoanggiang5@gmail.com>
2017-09-16 01:54:40 +0700

    Init app

---
+++ JSNLinkChecker/Main.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace JSNLinkChecker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.dtgUrl = new System.Windows.Forms.DataGridView();
+            this.Referer = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.URL = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.HrefText = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HeaderCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panelFooter = new System.Windows.Forms.Panel();
+            this.panelHeader.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgUrl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.Controls.Add(this.lblUrl);
+            this.panelHeader.Controls.Add(this.btnCheck);
+            this.panelHeader.Controls.Add(this.txtUrl);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(979, 46);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUrl.Location = new System.Drawing.Point(3, 15);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(46, 20);
+            this.lblUrl.TabIndex = 2;
+            this.lblUrl.Text = "URL:";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCheck.Enabled = false;
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(871, 12);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(96, 26);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Start";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.Location = new System.Drawing.Point(55, 12);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(804, 26);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.TextChanged += new System.EventHandler(this.txtUrl_TextChanged);
+            // 
+            // panelMain
+            // 
+            this.panelMain.Controls.Add(this.dtgUrl);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 46);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(979, 597);
+            this.panelMain.TabIndex = 1;
+            // 
+            // dtgUrl
+            // 
+            this.dtgUrl.AllowUserToDeleteRows = false;
+            this.dtgUrl.AllowUserToOrderColumns = true;
+            this.dtgUrl.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgUrl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgUrl.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Referer,
+            this.URL,
+            this.HrefText,
+            this.HeaderCode});
+            this.dtgUrl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dtgUrl.Location = new System.Drawing.Point(0, 0);
+            this.dtgUrl.Name = "dtgUrl";
+            this.dtgUrl.ReadOnly = true;
+            this.dtgUrl.Size = new System.Drawing.Size(979, 597);
+            this.dtgUrl.TabIndex = 0;
+            // 
+            // Referer
+            // 
+            this.Referer.HeaderText = "Referer";
+            this.Referer.Name = "Referer";
+            this.Referer.ReadOnly = true;
+            // 
+            // URL
+            // 
+            this.URL.HeaderText = "URL";
+            this.URL.Name = "URL";
+            this.URL.ReadOnly = true;
+            // 
+            // HrefText
+            // 
+            this.HrefText.HeaderText = "Href Text";
+            this.HrefText.Name = "HrefText";
+            this.HrefText.ReadOnly = true;
+            // 
+            // HeaderCode
+            // 
+            this.HeaderCode.HeaderText = "Header Code";
+            this.HeaderCode.Name = "HeaderCode";
+            this.HeaderCode.ReadOnly = true;
+            // 
+            // panelFooter
+            // 
+            this.panelFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelFooter.Location = new System.Drawing.Point(0, 543);
+            this.panelFooter.Name = "panelFooter";
+            this.panelFooter.Size = new System.Drawing.Size(979, 100);
+            this.panelFooter.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 643);
+            this.Controls.Add(this.panelFooter);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panelHeader);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "JSN Link Checker";
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.panelMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgUrl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Panel panelFooter;
+        private System.Windows.Forms.DataGridView dtgUrl;
+        private System.Windows.Forms.DataGridViewLinkColumn Referer;
+        private System.Windows.Forms.DataGridViewLinkColumn URL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HrefText;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HeaderCode;
+    }
+}
+
---
+++ JSNLinkChecker/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JSNLinkChecker
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUrl_TextChanged(object sender, EventArgs e)
+        {
+            if (txtUrl.Text.Trim() != "")
+            {
+                btnCheck.Enabled = true;
+            }
+            else
+            {
+                btnCheck.Enabled = false;
+            }
+        }
+    }
+}
